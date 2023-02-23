d6e3f0790198db038d81e0642f4acdec20d19585
Justin <57725347+juskek@users.noreply.github.com>
2023-02-23 18:26:45 +0000

    Added "Other Tech" in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -48,39 +48,17 @@ const Footer = ({ t }: any) => {
 
   return (
     <>
-      {/* <FooterSection>
+      <FooterSection>
         <Container>
           <Row justify="space-between">
             <Col lg={10} md={10} sm={12} xs={12}>
-              <Language>{t("Contact")}</Language>
-              <Large to="/">{t("Tell us everything")}</Large>
-              <Para>
-                {t(`Do you have any question? Feel free to reach out.`)}
-              </Para>
-              <a href="mailto:l.qqbadze@gmail.com">
-                <Chat>{t(`Let's Chat`)}</Chat>
-              </a>
-            </Col>
-            <Col lg={8} md={8} sm={12} xs={12}>
-              <Title>{t("Policy")}</Title>
-              <Large to="/" left="true">
-                {t("Application Security")}
-              </Large>
-              <Large left="true" to="/">
-                {t("Software Principles")}
-              </Large>
-            </Col>
-            <Col lg={6} md={6} sm={12} xs={12}>
-              <Empty />
-              <Large left="true" to="/">
-                {t("Support Center")}
-              </Large>
-              <Large left="true" to="/">
-                {t("Customer Support")}
+              <Language>{t("Other Tech")}</Language>
+              <Large to={{ pathname: "https://youtube.com/shorts/iS_kTXIUcFI?feature=share" }} target="_blank">
+                {t("Embedded C Robot")}
               </Large>
             </Col>
           </Row>
-          <Row justify="space-between">
+          {/* <Row justify="space-between">
             <Col lg={10} md={10} sm={12} xs={12}>
               <Empty />
               <Language>{t("Address")}</Language>
@@ -124,9 +102,9 @@ const Footer = ({ t }: any) => {
                 </LanguageSwitch>
               </LanguageSwitchContainer>
             </Col>
-          </Row>
+          </Row> */}
         </Container>
-      </FooterSection> */}
+      </FooterSection>
       <Extra>
         <Container border={true}>
           <Row
